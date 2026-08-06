0393a81e9d3b450996aa6a3dfabd47750e042c42
Executive_Eagle <christophershelley257@gmail.com>
2026-08-06 01:23:15 -0500

    Add Phase 9 staff workspace entry

---
+++ src/staff/staffEntry.jsx
@@ -0,0 +1,1 @@
+import React from'react';import{createRoot}from'react-dom/client';import StaffWorkspace from'./StaffWorkspace.jsx';createRoot(document.getElementById('root')).render(<React.StrictMode><StaffWorkspace/></React.StrictMode>);
