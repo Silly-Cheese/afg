91b369d926cc510e49c208b51080424c9a268aed
Executive_Eagle <christophershelley257@gmail.com>
2026-08-06 01:41:35 -0500

    Phase 12: add career center entrypoint

---
+++ src/careers/careerEntry.jsx
@@ -0,0 +1,1 @@
+import React from'react';import{createRoot}from'react-dom/client';import CareerCenter from'./CareerCenter.jsx';createRoot(document.getElementById('root')).render(<React.StrictMode><CareerCenter/></React.StrictMode>);
