15737e49861d128e3edca90fa164b5fc93eb56e8
Executive_Eagle <christophershelley257@gmail.com>
2026-08-06 01:16:10 -0500

    Add Phase 8 careers entry point

---
+++ src/staffing/staffingEntry.jsx
@@ -0,0 +1,1 @@
+import React from'react';import{createRoot}from'react-dom/client';import CareersPage from'./CareersPage.jsx';createRoot(document.getElementById('root')).render(<React.StrictMode><CareersPage/></React.StrictMode>);
